25e6e4fdc2f8fef924b1afaec4be746c9b850557
Navin <navin.prasath@sap.com>
2021-05-14 19:38:34 +0530

    initial

---
+++ centralProcessingEngine/src/test/java/com/getbullish/centralProcessingEngine/CentralProcessingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getbullish.centralProcessingEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralProcessingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

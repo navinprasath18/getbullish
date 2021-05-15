521c5427d01e4fd6b2f9f521017b132e16db9432
navinprasath18 <navin.prasath97@gmail.com>
2021-05-15 15:06:03 +0530

    Initalspring app (#3)
    * repo and service
    
    * added get repo
    
    Co-authored-by: Navin <navin.prasath@sap.com>

---
+++ centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/service/BaseServiceImpl.java
@@ -0,0 +1,39 @@
+package com.getbullish.centralProcessingEngine.service;
+
+import java.util.List;
+import java.util.UUID;
+import javax.persistence.EntityNotFoundException;
+import com.getbullish.centralProcessingEngine.Entities.EntityIdentity;
+
+public abstract class BaseServiceImpl<E extends EntityIdentity> implements BaseService<E> {
+
+
+
+  public E getEntity(UUID id) {
+    return getBaseRepository().findById(id)
+        .orElseThrow(() -> new EntityNotFoundException(id.toString()));
+  }
+
+
+
+  public List<E> list() {
+    return this.getBaseRepository().findAll();
+  }
+
+
+
+  public Boolean exist(UUID id) {
+
+    return null;
+  }
+
+
+
+  public void delete(UUID id) {
+
+
+  }
+
+
+
+}

b5e3a60df3880e8e1a0666f751c7d4584028f488
Navin <navin.prasath@sap.com>
2021-06-05 20:13:58 +0530

    patch3

--- centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/repos/StockDataRepo.java
+++ centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/repos/StockDataRepo.java
@@ -2,9 +2,13 @@ package com.getbullish.centralProcessingEngine.repos;
 
 import java.util.UUID;
 import org.springframework.stereotype.Repository;
+import com.getbullish.centralProcessingEngine.Entities.Stock;
 import com.getbullish.centralProcessingEngine.Entities.StockDataEntity;
 
 @Repository
 public interface StockDataRepo extends Baserepo<StockDataEntity, UUID> {
+
+
+  StockDataEntity findByStock(Stock stock);
 
 }
--- centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/scheduledThreads/PopulateStockPricesFromAvailableHistory.java
+++ centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/scheduledThreads/PopulateStockPricesFromAvailableHistory.java
@@ -1,11 +1,14 @@
 package com.getbullish.centralProcessingEngine.scheduledThreads;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.getbullish.centralProcessingEngine.Entities.HistoryEntity;
 import com.getbullish.centralProcessingEngine.Entities.Stock;
 import com.getbullish.centralProcessingEngine.Entities.StockDataEntity;
 import com.getbullish.centralProcessingEngine.repos.HistoryRepo;
+import com.getbullish.centralProcessingEngine.repos.StockDataRepo;
+import com.getbullish.centralProcessingEngine.repos.StockRepo;
 import com.getbullish.centralProcessingEngine.service.HistoryService;
 import com.getbullish.centralProcessingEngine.service.StockService;
 import lombok.extern.slf4j.Slf4j;
@@ -27,23 +30,42 @@ public class PopulateStockPricesFromAvailableHistory {
   @Autowired
   HistoryRepo historyrepo;
 
+  @Autowired
+  StockDataRepo stockdatarepo;
+
+
+  @Autowired
+  StockRepo stockrepo;
+
 
   public void populate() {
 
 
-    Stock stock = stockservice.findbySymbol("RELIANCE");
-    StockDataEntity entity = new StockDataEntity();
 
-    var history = historyservice.getHistoryListByStock(stock);
+    List<Stock> stocks = stockrepo.findAll();
+    log.info("---Stocks-- :" + stocks.size());
 
-    log.info("#Histry --Count :" + history.size());
+    for (Stock stock : stocks) {
+      try {
 
-    HistoryEntity lastknown = historyrepo.findFirstByStockOrderByDateDesc(stock);
-    log.info("-----" + lastknown.getDate());
+        StockDataEntity stockdataentity = stockdatarepo.findByStock(stock);
+        if (stockdataentity == null)
+          stockdataentity = new StockDataEntity();
+
+        HistoryEntity lastknown = historyrepo.findFirstByStockOrderByDateDesc(stock);
+        stockdataentity.setStock(stock);
+        stockdataentity.setHigh52week(lastknown.getHigh52week());
+        stockdataentity.setLow52week(lastknown.getLow52week());
+        stockdataentity.setPrice(lastknown.getClosePrice());
+        stockdatarepo.save(stockdataentity);
+        // log.info("-----" + lastknown.getDate());
+      } catch (Exception e) {
+
+        log.error("==#ERROR== :" + e.getMessage());
+
+      }
 
 
+    }
   }
-
-
-
 }

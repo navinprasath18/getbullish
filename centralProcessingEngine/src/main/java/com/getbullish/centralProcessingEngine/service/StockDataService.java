2a54758a0f5c21aa64fde365858cd6dc56164454
Navin <navin.prasath@sap.com>
2021-06-05 16:29:36 +0530

    patch 2

---
+++ centralProcessingEngine/src/main/java/com/getbullish/centralProcessingEngine/service/StockDataService.java
@@ -0,0 +1,8 @@
+package com.getbullish.centralProcessingEngine.service;
+
+import com.getbullish.centralProcessingEngine.Entities.StockDataEntity;
+import com.getbullish.centralProcessingEngine.data.StockDataDTO;
+
+public interface StockDataService extends BaseService<StockDataEntity, StockDataDTO> {
+
+}
